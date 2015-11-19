b7c27cf6f67babd878ddd6645327db6babb6471e
Andrey Tolmachev <andrey.tolmachev@gmail.com>
2015-11-19 22:46:57 +0300

    refactor kata

---
+++ refactor/src/Usages.java
@@ -0,0 +1,6 @@
+public class Usages {
+  public static void main(String[] args) {
+    Abbreviate.abbreviate("London is a capital of Great Britain", 0, 9);
+    Abbreviate.abbreviate("London is a capital of Great Britain", 7, 1000000);
+  }
+}
