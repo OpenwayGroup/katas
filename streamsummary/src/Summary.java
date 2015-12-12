c2673c738cced8bff0bd12e30c442a9fa27c19cc
Alexey Vladykin <vladykin@gmail.com>
2015-12-12 15:24:08 +0300

    added solution for streamsummary kata

--- streamsummary/src/Summary.java
+++ streamsummary/src/Summary.java
@@ -13,6 +13,42 @@ public class Summary<T> {
     }
 
     public static <T extends Comparable<T>> Summary<T> ofStream(Stream<? extends T> stream) {
-        return null; // write your code here
+        Accumulator<T> result = stream.collect(Accumulator::new, Accumulator::accept, Accumulator::combine);
+        return new Summary<>(result.min, result.max, result.count);
+    }
+
+    private static class Accumulator<T extends Comparable<T>> {
+
+        T min;
+        T max;
+        int count;
+
+        public void accept(T t) {
+            checkMin(t);
+            checkMax(t);
+            count++;
+        }
+
+        public void combine(Accumulator<T> other) {
+            if (other.min != null) {
+                checkMin(other.min);
+            }
+            if (other.max != null) {
+                checkMax(other.max);
+            }
+            count += other.count;
+        }
+
+        private void checkMin(T t) {
+            if (min == null || min.compareTo(t) > 0) {
+                min = t;
+            }
+        }
+
+        private void checkMax(T t) {
+            if (max == null || max.compareTo(t) < 0) {
+                max = t;
+            }
+        }
     }
 }
